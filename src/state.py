cd3b69183348ed4bcf5cae0779a149b4e9395d83
Jeff Roche <jkeauctions@gmail.com>
2022-09-03 13:12:36 -0400

    Commit of updates for states

--- src/state.py
+++ src/state.py
@@ -10,4 +10,11 @@ class State:
         for k, v in kwargs.items():
             setattr(self, k, v)
 
+    def cities(self, cursor):
+        query = f"""SELECT DISTINCT cities.* FROM cities
+                    WHERE state_id = {self.id}"""
+        cursor.execute(query)
+        city_records = cursor.fetchall()
+        cities = src.build_from_records(src.City, city_records)
 
+        return cities
